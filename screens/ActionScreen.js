8acd430dc2484619331a6de728406227d90b515b
cctenshii <SissiWang2003@gmail.com>
2026-06-02 13:42:52 +0200

    basic screen page added for "activity" page

---
+++ screens/ActionScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Text, View} from "react-native";
+
+export default function ActionScreen() {
+    return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white'}}>
+            {/*header*/}
+
+            <Text style={{fontSize: 24, marginBottom: 20, color: 'black'}}>Maak impact op jouw manier</Text>
+
+            {/*cards*/}
+
+            {/*nav*/}
+        </View>
+    );
+}
