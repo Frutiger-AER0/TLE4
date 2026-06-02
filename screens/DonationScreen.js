f23c609e5ac23b6b4e98a0b5460ee2a7eba5c3f2
cctenshii <SissiWang2003@gmail.com>
2026-06-02 13:47:00 +0200

    basic screen for donation page

---
+++ screens/DonationScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Text, View} from "react-native";
+
+export default function DonationScreen() {
+    return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white'}}>
+            {/*header*/}
+
+            <Text style={{fontSize: 24, marginBottom: 20, color: 'black'}}>Doneer!</Text>
+
+            {/*form*/}
+
+            {/*nav*/}
+        </View>
+    );
+}
