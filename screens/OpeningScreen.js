9f4ff221a05a652be080b1782c0e08d1b1f902b6
Amber De Koster <killboy1100@gmail.com>
2026-06-03 13:56:43 +0200

    registreer pagina en functionaliteit
    Took 8 minutes

--- screens/OpeningScreen.js
+++ screens/OpeningScreen.js
@@ -1,10 +1,11 @@
 import {Text, TouchableOpacity, View} from "react-native";
 import React from "react";
+import AppHeader from "../components/layout/AppHeader";
 
 export default function OpeningScreen({navigation}) {
     return (
         <View className="flex-1 items-center justify-center px-6 bg-offWhite">
-            {/*header*/}
+            <AppHeader/>
             <Text className="text-3xl font-bold mb-8 text-darkBlue">Welkom bij SupporT!</Text>
 
             <TouchableOpacity className="w-full max-w-md bg-blue py-3 rounded-lg items-center mb-4"
