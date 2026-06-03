fe40df3ce1cd0df1552704d3cf52043179bb1fae
Amber De Koster <killboy1100@gmail.com>
2026-06-03 11:05:29 +0200

    removed tailwind
    Took 26 minutes

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 import {NavigationContainer} from "@react-navigation/native";
 import RegistryScreen from "./screens/RegistryScreen";
 
-import "./global.css"
-
 export default function App() {
   return (
     <NavigationContainer>
