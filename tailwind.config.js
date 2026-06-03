34b3ed01f442b033fb37b54ec5b7e178fb42d48a
cctenshii <SissiWang2003@gmail.com>
2026-06-03 11:31:36 +0200

    styleguide colors added in nativewind with variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
-  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    // NOTE: Update this to include the paths to all files that contain Nativewind classes.
+    content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
+    presets: [require("nativewind/preset")],
+    theme: {
+        extend: {
+            colors: {
+                darkBlue: "#14213D",
+                blue: "#0047AB",
+                purple: "#7B2CBF",
+                lightPurple: "#CDB4DB",
+                yellow: "#F4C430",
+                offWhite: "#F8F9FA"
+            },
+        },
+    },
+    plugins: [],
 }
 
