d1e0afee3faff685179c68b319af4366743a7f33
cctenshii <SissiWang2003@gmail.com>
2026-06-04 11:44:55 +0200

    padding top and bottom

--- screens/ActionScreen.js
+++ screens/ActionScreen.js
@@ -12,11 +12,11 @@ export default function ActionScreen() {
     };
 
     return (
-        <ScrollView contentContainerStyle={{flexGrow: 1, alignItems: 'center'}}
+        <ScrollView contentContainerStyle={{flexGrow: 1, alignItems: 'center', paddingTop: 20, paddingBottom: 20}}
                     className="flex-1">
             <View className="flex-1 bg-offWhite px-4">
                 {/* Stickers */}
-                <Text className="text-xl font-bold text-darkBlue mt-5 mb-5">
+                <Text className="text-xl font-bold text-darkBlue mb-5">
                     Maak impact op jouw manier
                 </Text>
                 <View className="flex flex-col gap-y-5">
@@ -54,12 +54,11 @@ export default function ActionScreen() {
 
                         <View className="p-4">
                             <Text className="text-xl font-semibold text-darkBlue">
-                                Stickers ontwerpen
+                                Spandoeken ontwerpen
                             </Text>
 
                             <Text className="mt-1 text-sm text-darkBlue">
-                                Jouw ontwerp, onze productie. Verspreid de boodschap met stickers.
-                            </Text>
+                                Help mee met een spandoek. Lever een ontwerp aan. Wij regelen de rest. </Text>
 
                             <TouchableOpacity className="self-end mt-4 bg-purple-600 px-6 py-2 rounded-xl">
                                 <Text className="text-white">button</Text>
@@ -78,12 +77,12 @@ export default function ActionScreen() {
 
                         <View className="p-4">
                             <Text className="text-xl font-semibold text-darkBlue">
-                                Stickers ontwerpen
+                                Donaties
                             </Text>
 
                             <Text className="mt-1 text-sm text-darkBlue">
-                                Jouw ontwerp, onze productie. Verspreid de boodschap met stickers.
-                            </Text>
+                                Draag bij aan de beweging. Jouw donatie komt 100% terecht waar die het meeste impact
+                                maakt, bij jou doel. </Text>
 
                             <TouchableOpacity className="self-end mt-4 bg-purple-600 px-6 py-2 rounded-xl">
                                 <Text className="text-white">button</Text>
